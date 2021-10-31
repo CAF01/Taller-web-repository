9b528f1d7bb1491d2c76a54e244d9106796adb42
CAF01 <Christian_pria@hotmail.com>
2021-10-30 23:40:53 -0500

    complement student info

---
+++ taller-app/src/app/site/users/components/auto-update/auto-update.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-auto-update',
+  templateUrl: './auto-update.component.html',
+  styleUrls: ['./auto-update.component.css']
+})
+export class AutoUpdateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
